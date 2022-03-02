e21f0c8bc2d9d589e5c77777238bc4e7c0911ca3
xixi <xixi@cc.mm>
2022-03-02 23:25:40 +0800

    first commit

---
+++ src/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
+        target: 'http://apis.juhe.cn',
+        // 允许跨域
+        changeOrigin: true,
+        //   ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+    // proxyTable: {
+    //   // proxy all requests starting with /api to jsonplaceholder
+    //   '/api': {
+    //     target: 'http://apis.juhe.cn',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
+  }
+}
